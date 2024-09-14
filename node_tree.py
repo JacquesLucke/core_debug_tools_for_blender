3bfda2444bc8e317d66cdd0177eb9a6552ae1227
Jacques Lucke <mail@jlucke.com>
2024-09-14 14:06:40 +0200

    add zone body visualization

--- node_tree.py
+++ node_tree.py
@@ -1,6 +1,15 @@
 import bpy
 
 
+def show_dot_graph(dot_str):
+    import webbrowser
+    import urllib.parse
+
+    dot_str_encoded = urllib.parse.quote(dot_str)
+    url = f"https://dreampuf.github.io/GraphvizOnline/?presentation#{dot_str_encoded}"
+    webbrowser.open(url)
+
+
 class ShowLazyFunctionGraphOperator(bpy.types.Operator):
     bl_idname = "debug.show_lazy_function_graph"
     bl_label = "Show Lazy-Function Graph"
@@ -14,23 +23,49 @@ class ShowLazyFunctionGraphOperator(bpy.types.Operator):
         return True
 
     def execute(self, context):
-        import urllib.parse
-        import webbrowser
-
         orig_tree = context.space_data.edit_tree
         deg = context.view_layer.depsgraph
         eval_tree = deg.id_eval_get(orig_tree)
         dot_str = eval_tree.debug_lazy_function_graph()
 
-        dot_str_encoded = urllib.parse.quote(dot_str)
-        url = (
-            f"https://dreampuf.github.io/GraphvizOnline/?presentation#{dot_str_encoded}"
-        )
-        webbrowser.open(url)
+        if dot_str:
+            show_dot_graph(dot_str)
         return {"FINISHED"}
 
 
-classes = (ShowLazyFunctionGraphOperator,)
+class ShowZoneBodyLazyFunctionGraphOperator(bpy.types.Operator):
+    bl_idname = "debug.show_zone_body_lazy_function_graph"
+    bl_label = "Show Zone Body Lazy-Function Graph"
+
+    @classmethod
+    def poll(cls, context):
+        if context.area.type != "NODE_EDITOR":
+            return False
+        if context.space_data.edit_tree is None:
+            return False
+        if context.active_node is None:
+            return False
+        return True
+
+    def execute(self, context):
+        orig_tree = context.space_data.edit_tree
+        orig_node = context.active_node
+
+        deg = context.view_layer.depsgraph
+
+        eval_tree = deg.id_eval_get(orig_tree)
+        eval_node = eval_tree.nodes[orig_node.name]
+
+        dot_str = eval_node.debug_zone_body_lazy_function_graph()
+        if dot_str:
+            show_dot_graph(dot_str)
+        return {"FINISHED"}
+
+
+classes = (
+    ShowLazyFunctionGraphOperator,
+    ShowZoneBodyLazyFunctionGraphOperator,
+)
 
 
 def register():
